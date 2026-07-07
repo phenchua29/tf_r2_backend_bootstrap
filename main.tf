d2d2f3c07fbfebabc7811e00cf3e416d6e7eb4ba
Phen Chua <89496400+phenchua29@users.noreply.github.com>
2026-07-07 14:20:46 +0700

    feat: add an option to skip generating account api token for the bucket (#5)

--- main.tf
+++ main.tf
@@ -5,23 +5,26 @@ resource "cloudflare_r2_bucket" "backend" {
 }
 
 data "cloudflare_account_api_token_permission_groups_list" "r2_bucket_item_read" {
+  count      = var.generate_account_api_token ? 1 : 0
   account_id = var.cloudflare_account_id
   name       = "Workers%20R2%20Storage%20Bucket%20Item%20Read"
 }
 
 data "cloudflare_account_api_token_permission_groups_list" "r2_bucket_item_write" {
+  count      = var.generate_account_api_token ? 1 : 0
   account_id = var.cloudflare_account_id
   name       = "Workers%20R2%20Storage%20Bucket%20Item%20Write"
 }
 
 resource "cloudflare_account_token" "backend_bucket" {
+  count      = var.generate_account_api_token ? 1 : 0
   account_id = var.cloudflare_account_id
   name       = cloudflare_r2_bucket.backend.name
   policies = [{
     effect = "allow"
     permission_groups = [
-      { id = data.cloudflare_account_api_token_permission_groups_list.r2_bucket_item_read.result[0].id },
-      { id = data.cloudflare_account_api_token_permission_groups_list.r2_bucket_item_write.result[0].id },
+      { id = data.cloudflare_account_api_token_permission_groups_list.r2_bucket_item_read[0].result[0].id },
+      { id = data.cloudflare_account_api_token_permission_groups_list.r2_bucket_item_write[0].result[0].id },
     ]
     resources = jsonencode({
       "com.cloudflare.edge.r2.bucket.${var.cloudflare_account_id}_default_${cloudflare_r2_bucket.backend.name}" = "*"
